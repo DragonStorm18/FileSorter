7002e8460d7aa0a56fc644a626e71645db021945
DragonStorm18 <39398863+DragonStorm18@users.noreply.github.com>
2018-05-18 17:14:50 +1000

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSort = new System.Windows.Forms.Button();
+            this.CheckMonth = new System.Windows.Forms.RadioButton();
+            this.CheckDate = new System.Windows.Forms.RadioButton();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(153, 162);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(106, 33);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // CheckMonth
+            // 
+            this.CheckMonth.AutoSize = true;
+            this.CheckMonth.Location = new System.Drawing.Point(125, 51);
+            this.CheckMonth.Name = "CheckMonth";
+            this.CheckMonth.Size = new System.Drawing.Size(151, 21);
+            this.CheckMonth.TabIndex = 2;
+            this.CheckMonth.TabStop = true;
+            this.CheckMonth.Text = "Sort Files By Month";
+            this.CheckMonth.UseVisualStyleBackColor = true;
+            // 
+            // CheckDate
+            // 
+            this.CheckDate.AutoSize = true;
+            this.CheckDate.Location = new System.Drawing.Point(125, 78);
+            this.CheckDate.Name = "CheckDate";
+            this.CheckDate.Size = new System.Drawing.Size(168, 21);
+            this.CheckDate.TabIndex = 3;
+            this.CheckDate.TabStop = true;
+            this.CheckDate.Text = "Sort Files By Full Date\r\n";
+            this.CheckDate.UseVisualStyleBackColor = true;
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.AutoSize = true;
+            this.lblFinish.Location = new System.Drawing.Point(122, 123);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(0, 17);
+            this.lblFinish.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 253);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.CheckDate);
+            this.Controls.Add(this.CheckMonth);
+            this.Controls.Add(this.btnSort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FIle Sorter 2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.RadioButton CheckMonth;
+        private System.Windows.Forms.RadioButton CheckDate;
+        private System.Windows.Forms.Label lblFinish;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            CheckMonth.Checked = true;
+        }
+        
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lblFinish.Text = "Sorting Files Please Wait......";
+
+            DirectoryInfo info = new DirectoryInfo(Application.StartupPath + "\\");
+            FileInfo[] files = info.GetFiles().Where(j => !j.Extension.EndsWith("exe")).OrderBy(p => p.LastWriteTime).ToArray();
+
+            if (CheckDate.Checked == true)
+            {
+                SelectionDate();
+            }
+            else
+                if (CheckMonth.Checked == true)
+                {
+                    SelectionMonth();
+                }
+            //progressBar1.Minimum = 0;
+            //progressBar1.Maximum = files.Length;
+
+            if (files.Length == 0)
+            {
+                MessageBox.Show("There are no files to move");
+            }
+            //if (progressBar1.Value == files.Length)
+            //{
+                //MessageBox.Show("File Sorting Complete");
+                lblFinish.Text = "FILE SORTING COMPLETE";
+            //}
+        }
+
+        public DateTime GetDateTaken(Image targetImg)
+        {
+            //Property Item 36867 corresponds to the Date Taken
+            PropertyItem propItem = targetImg.GetPropertyItem(36867);
+            DateTime dtaken;
+            //Convert date taken metadata to a DateTime object
+            string sdate = Encoding.UTF8.GetString(propItem.Value).Trim();
+            string secondhalf = sdate.Substring(sdate.IndexOf(" "), (sdate.Length - sdate.IndexOf(" ")));
+            string firsthalf = sdate.Substring(0, 10);
+            firsthalf = firsthalf.Replace(":", "-");
+            sdate = firsthalf + secondhalf;
+            dtaken = DateTime.Parse(sdate);
+            return dtaken;
+        }
+
+        private void SelectionMonth()
+        {
+            DirectoryInfo info = new DirectoryInfo(Application.StartupPath + "\\");
+            FileInfo[] files = info.GetFiles().Where(j => !j.Extension.EndsWith("exe")).OrderBy(p => p.LastWriteTime).ToArray();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string from = Application.StartupPath + "\\" + files[i];
+                string dest = Application.StartupPath + "\\" + files[i].LastWriteTime.ToString("yyyy-MMMM") + "\\" + files[i];
+
+                Directory.CreateDirectory(Application.StartupPath + "\\" + files[i].LastWriteTime.ToString("yyyy-MMMM"));
+
+                if (File.Exists(dest))
+                {
+                    DialogResult dr = MessageBox.Show("The file you are trying to move " + " ( " + files[i] + " ) " + " already exists. Do you want to overide it?", "Overwrite Files", MessageBoxButtons.YesNo);
+
+                    if (dr == DialogResult.Yes)
+                    {
+                        File.Delete(dest);
+                    }
+                    else
+                    {
+                        if (dr == DialogResult.No)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                File.Move(from, dest);
+               // progressBar1.Value = progressBar1.Value + 1;
+                //progressBar1.Update();
+            }
+        }
+
+        private void SelectionDate()
+        {
+            DirectoryInfo info = new DirectoryInfo(Application.StartupPath + "\\");
+            FileInfo[] files = info.GetFiles().Where(j => !j.Extension.EndsWith("exe")).OrderBy(p => p.LastWriteTime).ToArray();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string from = Application.StartupPath + "\\" + files[i];
+                string dest = Application.StartupPath + "\\" + files[i].LastWriteTime.ToString("yyyy-MM-dd") + "\\" + files[i];
+
+                Directory.CreateDirectory(Application.StartupPath + "\\" + files[i].LastWriteTime.ToString("yyyy-MM-dd"));
+
+                if (File.Exists(dest))
+                {
+                    DialogResult dr = MessageBox.Show("The file you are trying to move " + " ( " + files[i] + " ) " + " already exists. Do you want to overide it?", "Overwrite Files", MessageBoxButtons.YesNo);
+
+                    if (dr == DialogResult.Yes)
+                    {
+                        File.Delete(dest);
+                    }
+                    else
+                    {
+                        if (dr == DialogResult.No)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                File.Move(from, dest);
+               // progressBar1.Value = progressBar1.Value + 1;
+                //ogressBar1.Update();
+            }
+        }
+
+        private void checkMonth_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+       
+
+
